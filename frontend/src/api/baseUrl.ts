21e61753947ccf76fc0dfa6f6dd16e07c4626f06
adarsh <adarshjithu10@gmail.com>
2024-11-24 20:06:34 +0530

    updated

--- frontend/src/api/baseUrl.ts
+++ frontend/src/api/baseUrl.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //http://localhost:3000
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://www.myarticle.shop",
     headers: {
         "Content-Type": "application/json",
     },
